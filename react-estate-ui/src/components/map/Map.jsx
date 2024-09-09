5361b838334b223a19dc4073d8788274df5ac4ad
Aditya Goswami <adityagoswami071@gmail.com>
2024-09-10 01:14:31 +0530

    feat: added singlepage static

--- react-estate-ui/src/components/map/Map.jsx
+++ react-estate-ui/src/components/map/Map.jsx
@@ -5,8 +5,8 @@ import "leaflet/dist/leaflet.css"
 import Pin from '../pin/Pin'
 
 
-export default function Map({item}) {
-    console.log(item)
+export default function Map({items}) {
+
     const position = [51.505, -0.09]
   return (
     <MapContainer className='map' center={position} zoom={6} scrollWheelZoom={false}>
@@ -14,7 +14,7 @@ export default function Map({item}) {
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-   {item.map(elem=>( <Pin key={elem.id} item={elem} /> ))}
+   {items.map(elem=>( <Pin key={elem.id} item={elem} /> ))}
   </MapContainer>
   )
 }
