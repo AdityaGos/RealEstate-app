77478821d96a004b1653279b3d679b116ce0cef8
Aditya Goswami <adityagoswami071@gmail.com>
2024-09-19 13:11:59 +0530

    feat: responsive card component

--- react-estate-ui/src/components/card/Card.jsx
+++ react-estate-ui/src/components/card/Card.jsx
@@ -1,42 +1,48 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Card.scss'
-export default function Card({item}) {
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Card.scss";
+export default function Card({ item }) {
+  const navigate = useNavigate();
   return (
-    <div className='card'>
-        <Link to={`/${item.id}`} className='imageContainer'>
+    <div className="card">
+      <div onClick={navigate(`/${item.id}`)} className="imageContainer">
         <img src={item.img} alt="" />
-        </Link>
-        <div className="textContainer">
-            <h2 className="title">
-                <Link to={`/${item.id}`} >{item.title}</Link>
-            </h2>
-            <p className="address">
-                <img src="/pin.png" alt="" />
-                <span>{item.address}</span>
-            </p>
-            <p className="price">$ {item.price}</p>
-            <div className="bottom">
-                <div className="features">
-                    <div className="feature">
-                        <img src="/bed.png" alt="" />
-                        <span>{item.bedroom} Bedrooms</span>
-                    </div>
-                    <div className="feature">
-                        <img src="/bed.png" alt="" />
-                        <span>{item.bathroom} Bathrooms</span>
-                    </div>
-                </div>
-                <div className="icons">
-                    <div className="icon">
-                        <img src="/save.png" alt="" />
-                    </div>
-                    <div className="icon">
-                        <img src="/chat.png" alt="" />
-                    </div>
-                </div>
+      </div>
+      <div className="textContainer">
+        <div className="titleContainer">
+          <h2 className="title">
+            <Link to={`/${item.id}`}>{item.title}</Link>
+          </h2>
+          <div className="icons">
+            <div className=" icon">
+              <img src="/save.png" alt="" />
             </div>
+          </div>
         </div>
+
+        <p className="address">
+          <img src="/pin.png" alt="" />
+          <span>{item.address}</span>
+        </p>
+        <p className="price">$ {item.price}</p>
+        <div className="bottom">
+          <div className="features">
+            <div className="feature">
+              <img src="/bed.png" alt="" />
+              <span>{item.bedroom} Bedroom{item.bedroom>1 ? 's':''}</span>
+            </div>
+            <div className="feature">
+              <img src="/bed.png" alt="" />
+              <span>{item.bathroom} Bathroom{item.bathroom>1 ? 's':''}</span>
+            </div>
+          </div>
+          <div className="icons">
+            <div className="icon">
+              <img src="/chat.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
