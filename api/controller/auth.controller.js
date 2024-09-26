37429c2826ed6ae143237e716a4bef63f1093177
Aditya Goswami <adityagoswami071@gmail.com>
2024-09-26 17:50:18 +0530

    feat: added authController & verifyToken middleware

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,16 +1,28 @@
 import bcrypt from "bcrypt";
 import prisma from "../lib/prisma.js";
 import jwt from 'jsonwebtoken'
+
+
+
+const ERROR={
+    User_email_key:"Email already in use ",
+    User_username_key:"Username already in use"
+}
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
   try {
     //hash password
-    if (!password || !email) {
-      throw new Error("Email/password missing ");
-    }
+    const existingUser = await prisma.user.findFirst({
+        where: {
+          OR: [{ username }, { email }]
+        }
+      });
+      ApiError
+    if(existingUser.username ==username) {return res.status(400).send({message:ERROR['User_username_key']})}
+    if(existingUser.email ==email){return res.status(400).send({message:ERROR['User_email_key']})}
     const hashedPassword = await bcrypt.hash(password, 10);
     //create a new user
-    const newUser = await prisma.user.create({
+   await prisma.user.create({
       data: {
         username: username,
         email: email,
@@ -20,48 +32,53 @@ export const register = async (req, res) => {
     res.status(201).json({ message: "User created successfully" });
 
   } catch (err) {
-    console.log(err);
+    // console.log(err.meta.target);
     res.status(500).json({ message: `${err}` });
   }
 
   //save it to database
 };
 export const login = async (req, res) => {
-    console.log("login called")
+ 
   const { username, password } = req.body;
 
-  //check if user exists
+  
   try {
+   
     const user = await prisma.user.findUnique({
       where: { username },
     })
+     //check if user exists
     if (!user) {
-      res.status(401).json({ message: "Invalid Credential" });
+      return res.status(401).json({ message: "User not Found" });
     }
 
      // match the password
 
     const isValidPassword = await bcrypt.compare(password, user.password);
     if (!isValidPassword) {
-      res.status(401).json({ message: "Invalid Credential" });
+      return res.status(401).json({ message: "Wrong password" });
     }
     const age = 1000*60*60*24*7
      //generate cookie token and send to user
 
      const token = jwt.sign(
-        {id:user.id}, 
+        {
+          id:user.id,
+          isAdmin:false
+        }, 
         process.env.JWT_SECRET_KEY,
         {expiresIn:age}
     )
-    console.log("jwt token")
-    console.log("jwt token", token)
+
+    const {password:userPassword, ...userInfo} = user
     //  res.setHeader("Set-Cookie","test","myValue")
 
     
     res.cookie("token",token,{
         httpOnly:true,
         maxAge:age
-    }).status(200).json({message:"Login successful"})
+    }).status(200).json(userInfo)
 
   } catch (err) {
     res.status(500).json({ message: err });
