5cf8e8bfad339f312627c6b555a6160031faaf1c
Aditya Goswami <adityagoswami071@gmail.com>
2024-10-10 14:59:43 +0530

    feat: added route

--- api/app.js
+++ api/app.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import postRoute from './routes/post.route.js'
 import authRoute from './routes/auth.route.js'
 import testRoute from './routes/test.route.js'
+import userRoute from './routes/user.route.js'
+import postRoute from './routes/post.route.js'
 import cors from 'cors'
 
 const app = express();
@@ -21,3 +22,5 @@ console.log("Server is running on 8800")
 // app.use('/api/test',postRoute)
 app.use('/api/auth',authRoute)
 app.use('/api/test',testRoute)
+app.use('/api/users',userRoute)
+app.use('/api/posts',postRoute)
